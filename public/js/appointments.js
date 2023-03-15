64cccc1d817a984fc22fa67a881917306a7bdc0b
Yury Veretenko <yura11v@gmail.com>
2023-03-15 12:38:04 +0200

    minor fix

--- public/js/appointments.js
+++ public/js/appointments.js
@@ -263,6 +263,7 @@ $(document).ready(function(){
                 edit_appointment_modal.find('select[name="vaccines\\[\\]"]').val(vaccines).multiselect("refresh");
                 edit_appointment_modal.find('input[name="neurology"]').prop('checked', row.neurology);
                 edit_appointment_modal.find('input[name="earlier"]').prop('checked', row.earlier);
+                edit_appointment_modal.find('input[name="call_back"]').prop('checked', row.call_back);
 
                 $('#created_at').text(row.created_at).closest('div').toggle(!!row.created_at);
                 $('#updated_at').text(row.updated_at).closest('div').toggle(!!row.updated_at);
