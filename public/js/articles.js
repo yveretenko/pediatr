2d06c9a25aa05c41ef5946b59d44d3fe89ca47d9
Yury Veretenko <yura11v@gmail.com>
2023-10-31 22:14:03 +0200

    minor fix

--- public/js/articles.js
+++ public/js/articles.js
@@ -2,14 +2,14 @@ let article_modal=$('#article_modal');
 
 function open_article(article_id)
 {
-    article_modal.modal('show');
-
     $.ajax({
         url: '/articles/get',
         data: {id: article_id},
         dataType: 'json',
         method: 'POST'
     }).done(function(data){
+        article_modal.modal('show');
+
         article_modal.find('h2').text(data.title);
         article_modal.find('.article_text').html(data.text);
         article_modal.find('#article_main_image').attr('src', '/img/articles/'+data.id+'.jpg');
