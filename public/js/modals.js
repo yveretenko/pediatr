7e4b5f83c732814827ec9cbe85a7577d5e8851fa
Yury Veretenko <yura11v@gmail.com>
2025-01-23 11:18:31 +0200

    payment buttons

--- public/js/modals.js
+++ public/js/modals.js
@@ -3,4 +3,9 @@ $(function(){
 
     if($(hash+'_modal').length>0)
         $(hash+'_modal').modal('show');
+
+    $('#try_again').click(function(){
+        $('#pay_fail_modal').modal('hide');
+        $('#pay_modal').modal('show');
+    });
 });
