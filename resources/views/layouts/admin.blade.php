cf1b05856d34337e3ede5ae0114b630e14a9abf8
Yury Veretenko <yura11v@gmail.com>
2025-09-21 23:15:13 +0300

    refactor: modularize navigation by extracting navbar into a separate partial

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,63 +21,8 @@
     <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg alert-dark p-0">
-    <div class="navbar-brand pl-3">
-        <A href="/" class="text-decoration-none text-dark">
-            pediatr.cv.ua
-        </A>
-    </div>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_content" aria-controls="navbar_content" aria-expanded="false">
-        <span><i class="fas fa-bars fa-lg text-muted"></i></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbar_content">
-        @auth
-        <ul class="navbar-nav">
-            <li class="nav-item px-3 px-md-1">
-                <A class="nav-link text-dark" href="/admin/appointments/">
-                    <i class="fa fa-list mr-1" aria-hidden="true"></i>
-                    Записи
-                </A>
-            </li>
-            <li class="nav-item px-3 px-md-1">
-                <A class="nav-link text-dark" href="/admin/appointments/graph/">
-                    <i class="fa fa-chart-bar mr-1" aria-hidden="true"></i>
-                    Статистика
-                </A>
-            </li>
-            <li class="nav-item px-3 px-md-1">
-                <A class="nav-link text-dark" href="/admin/appointments/files/">
-                    <i class="far fa-file-alt mr-1" aria-hidden="true"></i>
-                    Файли
-                </A>
-            </li>
-            <li class="nav-item px-3 px-md-1">
-                <A class="nav-link text-dark" href="/admin/vaccines/">
-                    <i class="fa fa-vial mr-1" aria-hidden="true"></i>
-                    Вакцини
-                </A>
-            </li>
-            <li class="nav-item px-3 px-md-1">
-                <A class="nav-link text-dark" href="/admin/dates-disabled/">
-                    <i class="far fa-calendar-times mr-1" aria-hidden="true"></i>
-                    Закриті дати
-                </A>
-            </li>
-        </ul>
-
-        <ul class="navbar-nav ml-auto p-0">
-            <li class="nav-item px-3 px-md-1">
-                <A class="nav-link text-dark" href="/admin/index/logout/">
-                    <i class="fa fa-sign-out-alt" aria-hidden="true"></i>
-                    Вийти
-                </A>
-            </li>
-        </ul>
-        @endguest
-    </div>
-</nav>
+@include('partials.admin.nav')
 
 <div class="col-12 col-xl-10 container-xl pb-5 px-1 px-md-4">
     <h5 class="ml-2 ml-md-0 my-4"><i class="@yield('title_icon')"></i> @yield('title')</h5>
